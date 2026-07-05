0bd4bbe055db436900a3f9367839d1b118ba469b
Jayden Yup <75883243+Joydurn@users.noreply.github.com>
2026-07-05 12:17:23 +0800

    Info screen update

--- js/game.js
+++ js/game.js
@@ -250,30 +250,28 @@ infoScene.create=function(){
         this.scene.start('home');
     },this)
     text = this.add.text(120, 20, 
-        `
-        Solve the puzzles as fast as you can, matching the 3 dials to the solution.
-        Everytime you load, the starting position is randomised.
+`
+Tool for RE2 remake speedrunners. Now Mobile-friendly c:
 
-        Controls:
-        W and S to go up and down the dial
-        A and D to switch dials
-        Q,E or F to complete puzzle
-        R to restart
-        P to clear PB
-        
-        Other notes:
-        Tried to account for input delay when going up/down dials: 
-        if down/up inputs are less than about 160ms apart, error will be displayed
-        when trying to complete the puzzle. This is to try to simualate how the game
-        can drop inputs if you spam too fast, definitely not accurate though.
-        
-        Game/tool is based on Resident Evil 2 Remake's medallion puzzles to help speedrunners 
+Solve the puzzles as fast as you can, matching the 3 dials to the solution.
 
-        Actual puzzles made by CAPCOM Kappa
-        
-        If got problems or suggestions or whatevers, can contact me at 
-        Joydurn Yup#3908 on discord or JoydurnYup on twitch`,
-        {fontFamily:'Trebuchet MS', fontSize: 26, color: '#FFFFFF'}).setOrigin(0,0);
+Every time you load, the starting position is randomised.
+
+GAUNTLET MODE: Cycle between the 3 medallions in order you would do them in the speedrun (A scenario: Lion-Unicorn-Maiden,  B scenario: Maiden-Unicorn-Lion)
+
+Controls:
+TOUCHSCREEN CONTROLS OR
+W and S or Arrow Up and Down to go up and down the dial
+A and D or Arrow Left and Right to switch dials
+Q,E ,F, or Left Click to complete puzzle
+R to reset
+P to clear PB
+
+Other notes:
+Tried to be close to the game feel of the inputs but do note its different in real game
+Super spagetti code so bugs here and there
+`
+    ,{fontFamily:'Trebuchet MS', fontSize: 26, color: '#FFFFFF'}).setOrigin(0,0);
     text.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5);
 
 }
